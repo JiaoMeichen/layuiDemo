86b06a489b02f44c4dd79ec774e857ddee5104ec
JiaoMeichen <1751022481@qq.com>
2018-04-27 18:34:28 +0800

    first

---
+++ datas/nav.js
@@ -0,0 +1,19 @@
+var navs = [{
+	"title": "基本元素",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "按钮",
+		"icon": "&#xe641;",
+		"href": "button.html"
+	}, {
+		"title": "表单",
+		"icon": "&#xe63c;",
+		"href": "form.html"
+	}]
+},{
+	"title": "这是一级导航",
+	"icon": "fa-stop-circle",
+	"href": "https://www.baidu.com",
+	"spread": false
+}];
